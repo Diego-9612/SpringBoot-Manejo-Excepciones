80ee1a92082185fa482d316a4e589db343e46eb2
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-09-30 09:55:27 -0500

    Primer commit del proyecto. se crea solo el proyecto en Spring-Boot

---
+++ src/main/java/como/diego/springboot/manejo/excepciones/springboot_manejo_excepciones/SpringbootManejoExcepcionesApplication.java
@@ -0,0 +1,13 @@
+package como.diego.springboot.manejo.excepciones.springboot_manejo_excepciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManejoExcepcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootManejoExcepcionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/como/diego/springboot/manejo/excepciones/springboot_manejo_excepciones/SpringbootManejoExcepcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package como.diego.springboot.manejo.excepciones.springboot_manejo_excepciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootManejoExcepcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
